a3fb806df6eb7424519be8dae1db440f2dd3ced8
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-08-19 14:12:16 -0400

    got colors to work

--- script.js
+++ script.js
@@ -1,40 +1,43 @@
 $(document).ready(function(){
     $('#currentDay').text(moment().format('dddd, MMMM Do'));
  
-    var hourBlocks = [{0: 0,
-    1 : $('#1'), 
-    2 : $('#2'), 
-    3 : $('#3'), 
-    4 : $('#4'), 
-    5 : $('#5'), 
-    6 : $('#6'), 
-    7 : 7, 
-    8 : 8, 
-    9 : $('#9'),
-    10 : $('#10'), 
-    11 :$('#11'), 
-    12 : $('#12'),
-    }];
-   
-    var currentHour = moment().format('h');
-   
-    for (var i = 1; i < 13; i++){
-        let hour = hourBlocks[0][i];
-        
-        if (parseInt(hour) === parseInt(currentHour)){        
-            $('.chngBackgrd').addClass("bg-danger");
-        }if (parseInt(hour) < parseInt(currentHour)){
-            $('.chngBackgrd').addClass("bg-secondary");
-        }if (parseInt(hour) > parseInt(currentHour)){
-            $('.chngBackgrd').addClass("bg-success");
-        };
-    };
+    let currentHour = moment().hours();
 
+    $(".hBlock").each(function (){
+        let hour = parseInt($(this).attr("id"));
+        console.log(hour);
+    
+        if (hour === currentHour) {
+            $(this).addClass("bg-danger");
+            $(this).removeClass("bg-success");
+            $(this).removeClass("bg-secondary");
+        }
+        else if (hour > currentHour) {
+            $(this).addClass("bg-success");
+            $(this).removeClass("bg-danger");
+            $(this).removeClass("bg-secondary");
+        }
+        else {
+            $(this).addClass("bg-secondary");
+            $(this).removeClass("bg-success");
+            $(this).removeClass("bg-danger");
+        }
+    });
+    
     $(".save").click(function(){   
-        var input = $(this).siblings(".chngBackgrd").val()
-        var whichHour = $(this).siblings(".chngBackgrd").attr("id")
+        var input = $(this).siblings(".hBlock").val()
+        var whichHour = $(this).siblings(".hBlock").attr("id")
         localStorage.setItem(whichHour, input);
     });
 
-    $(".chngBackgrd").
+    $("#9").val(localStorage.getItem("9"))
+    $("#10").val(localStorage.getItem("10"))
+    $("#11").val(localStorage.getItem("11"))
+    $("#12").val(localStorage.getItem("12"))
+    $("#13").val(localStorage.getItem("13"))
+    $("#14").val(localStorage.getItem("14"))
+    $("#15").val(localStorage.getItem("15"))
+    $("#16").val(localStorage.getItem("16"))
+    $("#17").val(localStorage.getItem("17"))
+    
 });
